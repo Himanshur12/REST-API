b33befddb9c52db631d62dd91b9cfc6936c0b377
Himanshur12 <himanshu.rawat@smartdatainc.net>
2020-02-19 05:02:05 +0530

    API

---
+++ data.js
@@ -0,0 +1,33 @@
+const data = [
+  {
+    "id": 1,
+    "first_name": "Himanshu",
+    "last_name": "Rawat",
+    "Age": "22",
+    
+  },
+  {
+    "id": 2,
+    "first_name": "Shivam",
+    "last_name": "Saini",
+    "Age": "21",
+    
+  },
+  {
+    "id": 3,
+    "first_name": "Ayush",
+    "last_name": "Cool",
+    "Age": "23",
+    
+  },
+  {
+    "id": 4,
+    "first_name": "Neeraj",
+    "last_name": "Rapido",
+    "Age": "24",
+    
+  },
+
+];
+
+module.exports = data;
